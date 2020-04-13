57a7fc6a25395a0a7012bc2e4c7697bec838d3ec
Thsiabner <thainvhe130209@fpt.edu.vn>
2020-04-13 10:08:45 +0700

    First cm

---
+++ src/Daemon/DaemonSocket.java
@@ -0,0 +1,21 @@
+package Daemon;
+
+public class DaemonSocket {
+	class DaemonSocketDownload{
+		
+		
+		//Your code in here
+	}
+	
+	
+	//////////////////////////
+	
+	
+	
+	class DaemonSocketUpload{
+		
+		//Your code in here
+		
+		
+	}
+}
---
+++ src/OriginalNode/Split.java
@@ -0,0 +1,5 @@
+package OriginalNode;
+
+public class Split {
+
+}
